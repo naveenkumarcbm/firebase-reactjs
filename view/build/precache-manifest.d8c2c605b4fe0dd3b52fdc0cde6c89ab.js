0a300abe242550b07640a5f9b09647ce5b89ca5c
Naveen Kumar <naveenkumar.murugan@hidglobal.com>
2020-05-13 11:36:24 +0530

    CSS init commmit

---
+++ view/build/precache-manifest.d8c2c605b4fe0dd3b52fdc0cde6c89ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46da5e80a922ea1ace389876b004d92d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75465e048fd99a4b7b77",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "010afa058aa82a2e0723",
+    "url": "/static/js/2.30993176.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/static/js/2.30993176.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75465e048fd99a4b7b77",
+    "url": "/static/js/main.d1a8e78a.chunk.js"
+  },
+  {
+    "revision": "e3e334c52dd6913bd8df",
+    "url": "/static/js/runtime-main.eb937c83.js"
+  }
+]);
